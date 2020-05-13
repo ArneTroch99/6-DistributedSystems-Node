0b22f136001025530c7a2fbb26ac549500912036
ArneTroch <44033131+ArneTroch@users.noreply.github.com>
2020-05-13 21:19:51 +0200

    print

--- src/main/java/be/uantwerpen/fti/ei/Distributed/project/Replication/Files.java
+++ src/main/java/be/uantwerpen/fti/ei/Distributed/project/Replication/Files.java
@@ -71,6 +71,7 @@ public class Files {
             if(!log.contains(node.getCurrentID())){
                 log.add(node.getCurrentID());
             }
+            System.out.println(fileName);
             fileLogs.put(fileName, log);
             logger.info("Successfully saved logs of file " + fileName);
         } catch (JsonProcessingException e) {

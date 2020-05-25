de29b038a8443c8711f49c804785f73f19a48403
Bas Calders <bas.calders@gmail.com>
2020-05-25 10:07:22 +0200

    Setup FailureAgent

---
+++ src/main/java/be/uantwerpen/fti/ei/Distributed/project/Agents/FailureAgent.java
@@ -0,0 +1,11 @@
+package be.uantwerpen.fti.ei.Distributed.project.Agents;
+
+import java.io.Serializable;
+
+public class FailureAgent implements Serializable, Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}

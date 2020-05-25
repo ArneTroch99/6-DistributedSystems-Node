6ad33a5e2b390e38555ff58d6c243a39d64613af
Bas Calders <bas.calders@gmail.com>
2020-05-25 10:39:43 +0200

    syncagent

--- src/main/java/be/uantwerpen/fti/ei/Distributed/project/Agents/FailureAgent.java
+++ src/main/java/be/uantwerpen/fti/ei/Distributed/project/Agents/FailureAgent.java
@@ -4,6 +4,14 @@ import java.io.Serializable;
 
 public class FailureAgent implements Serializable, Runnable{
 
+    private int failingID, currentID;
+    private int startID;
+
+    FailureAgent(int failingID, int currentID){
+        this.currentID = currentID;
+        this.failingID = failingID;
+    }
+
     @Override
     public void run() {
 
---
+++ src/main/java/be/uantwerpen/fti/ei/Distributed/project/Agents/SyncAgent.java
@@ -0,0 +1,16 @@
+package be.uantwerpen.fti.ei.Distributed.project.Agents;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SyncAgent implements Serializable, Runnable {
+
+    private Map<String, Integer> agentList = new HashMap<>();
+
+
+    @Override
+    public void run() {
+
+    }
+}

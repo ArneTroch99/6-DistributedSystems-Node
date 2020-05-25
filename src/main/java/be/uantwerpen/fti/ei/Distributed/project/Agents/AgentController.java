f0164fb03bc0e3c81eead2fc4f0823db47e007a3
NicolasGrupping <nicolas.grupping@student.uantwerpen.be>
2020-05-25 17:10:19 +0200

    httpTest

--- src/main/java/be/uantwerpen/fti/ei/Distributed/project/Agents/AgentController.java
+++ src/main/java/be/uantwerpen/fti/ei/Distributed/project/Agents/AgentController.java
@@ -3,7 +3,9 @@ package be.uantwerpen.fti.ei.Distributed.project.Agents;
 import be.uantwerpen.fti.ei.Distributed.project.Node;
 import be.uantwerpen.fti.ei.Distributed.project.fileProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -14,15 +16,18 @@ public class AgentController {
 
     private Node node;
     private Map<String, fileProperties> agentList;
+    private RestTemplateBuilder restTemplateBuilder;
+
 
     @Autowired
-    public AgentController(Node node){
+    public AgentController(RestTemplateBuilder restTemplateBuilder, Node node){
         this.node = node;
+        this.restTemplateBuilder = restTemplateBuilder;
     }
 
     @PostConstruct
     private void syncThread(){
-        Thread agentSync = new Thread(new SyncAgent(this.node));
+        Thread agentSync = new Thread(new SyncAgent(this.restTemplateBuilder, this.node));
         agentSync.start();
     }
 
--- src/main/java/be/uantwerpen/fti/ei/Distributed/project/Agents/SyncAgent.java
+++ src/main/java/be/uantwerpen/fti/ei/Distributed/project/Agents/SyncAgent.java
@@ -2,6 +2,7 @@ package be.uantwerpen.fti.ei.Distributed.project.Agents;
 
 import be.uantwerpen.fti.ei.Distributed.project.Node;
 import be.uantwerpen.fti.ei.Distributed.project.fileProperties;
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.AMSService;
@@ -9,6 +10,9 @@ import jade.domain.FIPAAgentManagement.AMSAgentDescription;
 import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,28 +22,29 @@ public class SyncAgent extends Agent {
 
     private final File localFolder = new File("Replication/LocalData");
     private File replicatedFolder = new File("Replication/ReplicatedData");
-    private final int nodeID;
+    private final int nodeID, nextNodeID;
     private final HashMap<String, fileProperties> agentList;
+    private final String namingServerIP;
+    private final RestTemplate restTemplate;
 
-    public SyncAgent(Node node) {
+    public SyncAgent(RestTemplateBuilder restTemplateBuilder, Node node) {
+        this.restTemplate = restTemplateBuilder.build();
         this.nodeID = node.getCurrentID();
+        this.nextNodeID = node.getNextID();
         this.agentList = node.getFileList();
+        this.namingServerIP = node.getNamingServerIp();
     }
 
 
     @Override
     protected void setup(){
-        AMSAgentDescription[] agents = null;
-        try {
-            SearchConstraints c = new SearchConstraints();
-            c.setMaxResults((long) -1);
-            agents = AMSService.search(this, new AMSAgentDescription(), c);
-        } catch (FIPAException e) {
-            e.printStackTrace();
-        }
         addBehaviour(new CyclicBehaviour(this) {
             @Override
             public void action() {
+                final String namingServerURL = "http://" + namingServerIP + ":8081/nodeip?id=" + nextNodeID;
+                ResponseEntity<String> nextNodeIP = restTemplate.getForEntity(namingServerURL, String.class);
+                System.out.println(nextNodeID);
+                //logger.info("Received ip address of node " + ID);
                 List<String> localFileNames = new ArrayList<>();
                 File[] localFiles = localFolder.listFiles();
                 Boolean changed = false;
